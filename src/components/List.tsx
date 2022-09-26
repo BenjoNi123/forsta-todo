dafb98c966a1963a51ca3ba028d17da93467b4dc
BenjoNi123 <benjamin.kukuljac@hotmail.com>
2022-09-26 17:54:01 +0200

    create todos react app

---
+++ src/components/List.tsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+const List = () => {
+    const [todos, setTodos] = useState([])
+    return (
+        <div><h1>HEPEEEEEEEEEEK</h1></div>
+    )
+}
+
+export default List;
